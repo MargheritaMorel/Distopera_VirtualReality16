d716efef614fb92704c3dd2cad4402775c9cb325
Brigitta Errera <brigittaerrera@gmail.com>
2024-02-12 22:38:06 +0100

    OptionMenu

--- Assets/Scripts/Bri/MainMenu.cs
+++ Assets/Scripts/Bri/MainMenu.cs
@@ -9,19 +9,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
+    public void QuitGame(){
+        Application.Quit();
+    }
 
 
-
-
-    // Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
-
-    // Update is called once per frame
-   // void Update()
-   // {
-        
-    //}
 }
